d0ec2f9523e2ba513dd822ca9b9dad15f9e1fe98
Baraly <79365734+Baraly@users.noreply.github.com>
2022-06-17 21:35:54 +0200

    Update bdd.php

--- function/bdd.php
+++ function/bdd.php
@@ -1,6 +1,5 @@
 <?php
 
 $bdd = new PDO('mysql:host=localhost;dbname=HorairePapa;charset=utf8','root','root');
-//$bdd = new PDO('mysql:host=185.98.131.158;dbname=baral1699398_1ohwgn;charset=utf8','baral1699398','ikgvmuffgr');
 
 ?>
